89f88047bf035c1d7f9eef058a53cf7614f73e01
Evgenij <semenov.evgeny.92@gmail.com>
2018-08-01 11:28:32 +0700

    Add local storage

--- src/reducer/taskslist.js
+++ src/reducer/taskslist.js
@@ -1,15 +1,29 @@
-import { tasks as defaultTaskList } from "../tasks.json";
+// import { tasks as defaultTaskList } from "../tasks.json";
 import { ADD_TASK, DEL_TASK } from "../constants/constants";
 
-export default (tasklist = defaultTaskList, action) => {
+export default (tasklist = loadFromLocalStorage('taskslist'), action) => {
   const { type, payload } = action;
+  var templist
 
   switch (type) {
     case ADD_TASK:
-      return [...tasklist, payload.task];
+      templist = [...tasklist, payload.task]
+      saveToLocalStorage(templist, 'taskslist')
+      return templist;
     case DEL_TASK:
-      return tasklist.filter(task => task.id !== payload.id);
+      templist = tasklist.filter(task => task.id !== payload.id)
+      saveToLocalStorage(templist, 'taskslist')
+      return templist;
     default:
       return tasklist;
   }
 };
+
+function saveToLocalStorage(data, name) {
+  return localStorage.setItem(name, JSON.stringify(data))
+}
+
+function loadFromLocalStorage(name) {
+  if(!localStorage[name]) saveToLocalStorage({}, name)
+  return JSON.parse(localStorage.getItem(name))
+}
